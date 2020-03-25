9466f1a7752c715028049ad92c0ba35f5062a070
denis303 <dev@denis303.com>
2020-03-26 01:07:51 +0300

    n/a

--- Config/BaseLiquid.php
+++ Config/BaseLiquid.php
@@ -5,12 +5,12 @@ namespace Denis303\Liquid\Config;
 abstract class BaseLiquid extends \CodeIgniter\Config\Config
 {
 
-    //public $INCLUDE_SUFFIX = '';
+    public $INCLUDE_SUFFIX = '';
 
-    //public $INCLUDE_PREFIX = '';
+    public $INCLUDE_PREFIX = '';
 
-    //public $INCLUDE_ALLOW_EXT = true;
+    public $INCLUDE_ALLOW_EXT = true;
 
-    //public $ESCAPE_BY_DEFAULT = true;
+    public $ESCAPE_BY_DEFAULT = true;
 
 }
